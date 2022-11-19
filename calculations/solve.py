Added precision in calculations. Small fixes in calculations.

calculations/solve.py
@@ -1,11 +1,11 @@
-from typing import Callable
+from typing import Callable, Tuple
 import numpy as np
-from calculations import y_infinity, A, Y_slash, A_v, P, u_0, u_G, y_0, y_G, y
+from calculations import y_infinity, A, Y_slash, A_v, P, u_0, u_G, y_0, y_G, y, precision
 
 
 def solve(G: Callable, u: Callable, S0: np.array, T: float,
           Lr0_list: np.array, xl0_list: np.array, LrG_list: np.array, slG_list: np.array,
-          v_0: Callable, v_G: Callable) -> Callable:
+          v_0: Callable, v_G: Callable) -> Tuple[Callable, float]:
     """
 
     :param G: function of two variables x, t - Green's function
@@ -18,7 +18,7 @@ def solve(G: Callable, u: Callable, S0: np.array, T: float,
     :param slG_list: list of slG that is np.array of two float values x and t: [[x0, t0], [x1, t1], ...]
     :param v_0: function of two variables x, t
     :param v_G: function of two variables x, t
-    :return: function of 2 variables x, t
+    :return: tuple of function of 2 variables x, t and float precision
     """
 
     res_y_infinity = y_infinity(G, u, S0, T)
@@ -27,9 +27,11 @@ def solve(G: Callable, u: Callable, S0: np.array, T: float,
     res_A_v = A_v(res_A, v_0, v_G, S0, T)
     res_P = P(res_A, S0, T)
     res_u_0 = u_0(res_A, res_P, res_Y_slash, res_A_v, v_0)
-    res_u_G = u_0(res_A, res_P, res_Y_slash, res_A_v, v_G)
+    res_u_G = u_G(res_A, res_P, res_Y_slash, res_A_v, v_G)
     res_y_0 = y_0(G, S0, T, res_u_0)
     res_y_G = y_G(G, S0, T, res_u_G)
     res_y = y(res_y_infinity, res_y_0, res_y_G)
 
-    return res_y
+    res_precision = precision(res_Y_slash, res_P)
+
+    return res_y, res_precision

calculations/u_G.py
@@ -25,6 +25,6 @@ def u_G(A_matrix: np.array, P: np.array, Y_slash: np.array, A_v: np.array, v_G: 
 
     def res(x: float, t: float) -> float:
         matrix_part = A_G(x, t) @ P_inv @ (Y_slash - A_v)  # Matrix 1x1
-        return matrix_part[0] + v_G(x, t)
+        return matrix_part[0][0] + v_G(x, t)
 
     return res

main.py
@@ -31,30 +31,37 @@ if __name__ == "__main__":
     # ------------------------------------------------------------------------------------------------------------------
 
     # Calculations testing
-    # def G(x_, t_):
-    #     return x_ ** 2
-    #
-    #
-    # def u(x_, t_):
-    #     return x_ * t_
-    #
-    #
-    # S0 = np.array([[0, 1]])
-    # T = 1
-    #
-    # Lr0_list = np.array([differential_operator(0, 1)])
-    # # print(operator(0, 1)(lambda x_, t_: x_ + x_*t_, (1, 1)))
-    # xl0_list = np.array([0.5, 1, 0.3])
-    # LrG_list = np.array([differential_operator(1, 1)])
-    # slG_list = np.array([[0.5, 1],
-    #                      [0.25, 0.75]])
-    #
-    # v_0 = lambda x_, t_: x_ + t_
-    # v_G = lambda x_, t_: x_ - t_
-    #
-    # res_solve = solve(G, u, S0, T, Lr0_list, xl0_list, LrG_list, slG_list, v_0, v_G)
-    #
-    # print(res_solve)
+    # def G(x, t):
+    #     #     if t <= 5:
+    #     #         return 0
+    #     #     else:
+    #     #         return parse_function("(1/sqrt(4*pi*t))*exp(-x^2/(4*t))")(x, t)
+
+    def G(x, t):
+        return x**2 + x * t
+
+
+    def u(x, t):
+        return x * t
+
+
+    S0 = np.array([[0, 1]])
+    T = 1
+
+    Lr0_list = np.array([differential_operator(0, 1)])
+    xl0_list = np.array([0.5, 1, 0.3])
+    LrG_list = np.array([differential_operator(1, 1)])
+    slG_list = np.array([[0.5, 1],
+                         [0.25, 0.75]])
+
+    v_0 = lambda x_, t_: x_ + t_
+    v_G = lambda x_, t_: x_ - t_
+
+    res_solve, res_precision = solve(G, u, S0, T, Lr0_list, xl0_list, LrG_list, slG_list, v_0, v_G)
+
+    print(f'Solve function: {res_solve}')
+    print(f'Precision: {res_precision}')
+    print()
 
     # ------------------------------------------------------------------------------------------------------------------
 
@@ -79,9 +86,8 @@ if __name__ == "__main__":
     print(f'Function value in ({x1}, {t1}): {res_parse_function1(x1, t1)}')
     print()
 
+    # function_str2 = "Piecewise((0, t<=0), ((1/sqrt(4*pi*t))*exp(-x^2/(4*t)), t>0))"
     function_str2 = "(1/sqrt(4*pi*t))*exp(-x^2/(4*t))"
-    # function_str2 = "(Piecewise((0, t<=0),(1, t>0))(t)/sqrt(4*pi*t))*exp(-x^2/(4*t))"
-    # function_str2 = "(Heaviside(t)/sqrt(4*pi*t))*exp(-x^2/(4*t))"
     print(f'Function string: {function_str2}')
     res_parse_function2 = parse_function(function_str2)
     x2, t2 = math.pi / 2, 1